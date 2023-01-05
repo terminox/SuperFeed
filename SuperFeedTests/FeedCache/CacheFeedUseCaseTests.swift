6a3a63e075658df669fbed53dae6442709baccc9
Yossa Bourne <cybercanonx@gmail.com>
2023-01-05 22:04:15 +0700

    test&refactor: refactor SUT creation to a factory method to protect tests against breaking changes

--- SuperFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ SuperFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -7,21 +7,69 @@
 
 import XCTest
 
+// MARK: - LocalFeedLoader
+
 class LocalFeedLoader {
+
+  // MARK: Lifecycle
+
   init(store: FeedStore) {
-    
+    self.store = store
   }
+
+  // MARK: Internal
+
+  func save(_: [FeedItem]) {
+    store.deleteCachedFeed()
+  }
+
+  // MARK: Private
+
+  private let store: FeedStore
 }
+
+// MARK: - FeedStore
 
 class FeedStore {
   var deleteCachedFeedCallCount = 0
+
+  func deleteCachedFeed() {
+    deleteCachedFeedCallCount += 1
+  }
 }
 
+// MARK: - CacheFeedUseCaseTests
+
 class CacheFeedUseCaseTests: XCTestCase {
-  
+
+  // MARK: Internal
+
   func test_init_doesNotDeleteCacheUponCreation() {
-    let store = FeedStore()
-    _ = LocalFeedLoader(store: store)
+    let (_, store) = makeSUT()
     XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+  }
+
+  func test_save_requestCacheDeletion() {
+    let items = [uniqueItem(), uniqueItem()]
+    let (sut, store) = makeSUT()
+
+    sut.save(items)
+    XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+  }
+
+  // MARK: Private
+  
+  private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+    let store = FeedStore()
+    let sut = LocalFeedLoader(store: store)
+    return (sut, store)
+  }
+
+  private func uniqueItem() -> FeedItem {
+    FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+  }
+
+  private func anyURL() -> URL {
+    URL(string: "http://any-url.com")!
   }
 }
