af3478970f3bc647bae37e730e48e5bdbcd7f191
Yossa Bourne <cybercanonx@gmail.com>
2023-01-06 12:18:25 +0700

    test: add a test to ensure SUT does not request cache insertion on deletion error

--- SuperFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ SuperFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -32,9 +32,13 @@ class LocalFeedLoader {
 
 class FeedStore {
   var deleteCachedFeedCallCount = 0
+  var insertCallCount = 0
 
   func deleteCachedFeed() {
     deleteCachedFeedCallCount += 1
+  }
+  
+  func completeDeletion(with error: Error, at index: Int = 0) {
   }
 }
 
@@ -56,12 +60,25 @@ class CacheFeedUseCaseTests: XCTestCase {
     sut.save(items)
     XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
   }
+  
+  func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+    let items = [uniqueItem(), uniqueItem()]
+    let (sut, store) = makeSUT()
+    let deletionError = anyNSError()
+
+    sut.save(items)
+    store.completeDeletion(with: deletionError)
+    
+    XCTAssertEqual(store.insertCallCount, 0)
+  }
 
   // MARK: Private
   
-  private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+  private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
     let store = FeedStore()
     let sut = LocalFeedLoader(store: store)
+    trackForMemoryLeaks(store, file: file, line: line)
+    trackForMemoryLeaks(sut, file: file, line: line)
     return (sut, store)
   }
 
@@ -71,5 +88,9 @@ class CacheFeedUseCaseTests: XCTestCase {
 
   private func anyURL() -> URL {
     URL(string: "http://any-url.com")!
+  }
+  
+  private func anyNSError() -> NSError {
+    NSError(domain: "any error", code: 0)
   }
 }
