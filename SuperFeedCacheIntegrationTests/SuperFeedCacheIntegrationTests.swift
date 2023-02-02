edd7d0579138f974374ed73b829726286041602e
Yossa Bourne <cybercanonx@gmail.com>
2023-02-02 14:06:28 +0700

    add SuperFeedCacheIntegrationTests target

---
+++ SuperFeedCacheIntegrationTests/SuperFeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  SuperFeedCacheIntegrationTests.swift
+//  SuperFeedCacheIntegrationTests
+//
+//  Created by yossa on 2/2/2566 BE.
+//
+
+import XCTest
+import SuperFeed
+
+final class SuperFeedCacheIntegrationTests: XCTestCase {
+  
+}
