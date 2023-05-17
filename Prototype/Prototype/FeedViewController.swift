dd58aff4b14366b2bfce356566b197187812fdc7
Yossa Bourne <cybercanonx@gmail.com>
2023-05-17 10:44:57 +0700

    feat: add prototype storyboard

---
+++ Prototype/Prototype/FeedViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FeedViewController.swift
+//  Prototype
+//
+//  Created by yossa on 17/5/2566 BE.
+//
+
+import UIKit
+
+final class FeedViewController: UITableViewController {
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    10
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath)
+  }
+}
