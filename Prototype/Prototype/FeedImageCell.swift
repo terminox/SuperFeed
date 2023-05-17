f79d8daac14e0d2a62cd89885dcbdd895c7acec8
Yossa Bourne <cybercanonx@gmail.com>
2023-05-17 11:11:54 +0700

    feat: add FeedImageCell with storyboard link

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by yossa on 17/5/2566 BE.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+  @IBOutlet private(set) var locationContainer: UIView!
+  @IBOutlet private(set) var locationLabel: UIView!
+  @IBOutlet private(set) var feedImageView: UIImageView!
+  @IBOutlet private(set) var descriptionLabel: UILabel!
+}
