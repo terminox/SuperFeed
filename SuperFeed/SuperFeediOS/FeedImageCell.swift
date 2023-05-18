44b3c042cb29d24e2bf2dfdda33ac66aa8bedec7
Yossa Bourne <cybercanonx@gmail.com>
2023-05-18 17:07:53 +0700

    test: add teste

---
+++ SuperFeed/SuperFeediOS/FeedImageCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedImageCell.swift
+//  SuperFeediOS
+//
+//  Created by yossa on 18/5/2566 BE.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+  public let locationContainer = UIView()
+  public let locationLabel = UILabel()
+  public let descriptionLabel = UILabel()
+  public let feedImageContainer = UIView()
+  public let feedImageView = UIImageView()
+}
--- SuperFeed/SuperFeediOS/FeedViewController.swift
+++ SuperFeed/SuperFeediOS/FeedViewController.swift
@@ -11,6 +11,7 @@ import SuperFeed
 final public class FeedViewController: UITableViewController {
   
   private var loader: FeedLoader?
+  private var tableModel: [FeedImage] = []
   
   public convenience init(loader: FeedLoader) {
     self.init()
@@ -30,8 +31,29 @@ final public class FeedViewController: UITableViewController {
   private func load() {
     refreshControl?.beginRefreshing()
     
-    loader?.load { [weak self] _ in
+    loader?.load { [weak self] result in
+      switch result {
+      case .success(let models):
+        self?.tableModel = models
+      case .failure(_):
+        break
+      }
+      
+      self?.tableView.reloadData()
       self?.refreshControl?.endRefreshing()
     }
+  }
+  
+  public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    tableModel.count
+  }
+  
+  public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cellModel = tableModel[indexPath.row]
+    let cell = FeedImageCell()
+    cell.locationContainer.isHidden = (cellModel.location == nil)
+    cell.locationLabel.text = cellModel.location
+    cell.descriptionLabel.text = cellModel.description
+    return cell
   }
 }
