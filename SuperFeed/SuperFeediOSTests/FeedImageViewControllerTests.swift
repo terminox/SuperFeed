f6a1dbcf3532e44a5d8609cf1337a0f96ab8736d
Yossa Bourne <cybercanonx@gmail.com>
2023-06-29 02:57:35 +0700

    reuse table view cell from storyboard configuration

--- SuperFeed/SuperFeediOSTests/FeedImageViewControllerTests.swift
+++ SuperFeed/SuperFeediOSTests/FeedImageViewControllerTests.swift
@@ -235,6 +235,17 @@ final class FeedImageViewControllerTests: XCTestCase {
     XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected second cancelled image URL request once second image is not near visible anymore")
   }
   
+  func test_feedImageView_doesNotRenderLoadedImageWhenNotVisibleAnymore() {
+    let (sut, loader) = makeSUT()
+    sut.loadViewIfNeeded()
+    loader.completeFeedLoading(with: [makeImage()])
+    
+    let view = sut.simulateFeedImageViewNotVisible(at: 0)
+    loader.completeImageLoading(with: UIImage.make(withColor: .red).pngData()!)
+    
+    XCTAssertNil(view?.renderedImage, "Expected no rendered image when an image load finishes after the view is not visible anymore")
+  }
+  
   // MARK: - Helpers
   
   private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -341,11 +352,13 @@ private extension FeedViewController {
     feedImageView(at: index) as? FeedImageCell
   }
   
-  func simulateFeedImageViewNotVisible(at row: Int) {
+  @discardableResult
+  func simulateFeedImageViewNotVisible(at row: Int) -> FeedImageCell? {
     let view = simulateFeedImageViewVisible(at: row)
     let delegate = tableView.delegate
     let index = IndexPath(row: row, section: feedImagesSection)
     delegate?.tableView?(tableView, didEndDisplaying: view!, forRowAt: index)
+    return view
   }
   
   func simulateFeedImageViewNearVisible(at row: Int) {
