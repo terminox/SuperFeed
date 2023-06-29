dd9debe393964285ed665e7688b30b247314f70d
Yossa Bourne <cybercanonx@gmail.com>
2023-06-30 01:43:14 +0700

    dispatch completion to main thread

--- SuperFeed/SuperFeediOS/UI/Controllers/FeedViewController.swift
+++ SuperFeed/SuperFeediOS/UI/Controllers/FeedViewController.swift
@@ -27,7 +27,13 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
   
   var tableModel: [FeedImageCellController] = [] {
     didSet {
-      tableView.reloadData()
+      if Thread.isMainThread {
+        tableView.reloadData()
+      } else {
+        DispatchQueue.main.async { [weak self] in
+          self?.tableView.reloadData()
+        }
+      }
     }
   }
   
@@ -37,6 +43,10 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
   }
   
   func display(_ viewModel: FeedLoadingViewModel) {
+    guard Thread.isMainThread else {
+      return DispatchQueue.main.async { [weak self] in self?.display(viewModel) }
+    }
+    
     if viewModel.isLoading {
       refreshControl?.beginRefreshing()
     } else {
