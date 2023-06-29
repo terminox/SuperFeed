7ac4857f4449f9261eec0f6bce4deb50bc9b3166
Yossa Bourne <cybercanonx@gmail.com>
2023-06-30 02:13:23 +0700

    dispatch image loading to main thread

--- SuperFeed/SuperFeediOS/UI/Composers  /FeedUIComposer.swift
+++ SuperFeed/SuperFeediOS/UI/Composers  /FeedUIComposer.swift
@@ -16,7 +16,9 @@ public final class FeedUIComposer {
     let presentationAdapter = FeedLoaderPresentationAdapter(feedLoader: MainQueueDispatchDecorator(decoratee: feedLoader))
     let feedController = FeedViewController.makeWith(delegate: presentationAdapter, title: FeedPresenter.title)
     presentationAdapter.presenter = FeedPresenter(
-      feedView: FeedViewAdapter(controller: feedController, imageLoader: imageLoader),
+      feedView: FeedViewAdapter(
+        controller: feedController,
+        imageLoader: MainQueueDispatchDecorator(decoratee: imageLoader)),
       loadingView: WeakRefVirtualProxy(feedController))
     return feedController
   }
@@ -42,6 +44,16 @@ private final class MainQueueDispatchDecorator<T> {
 extension MainQueueDispatchDecorator: FeedLoader where T == FeedLoader {
   func load(completion: @escaping (LoadFeedResult) -> Void) {
     decoratee.load { [weak self] result in
+      self?.dispatch {
+        completion(result)
+      }
+    }
+  }
+}
+
+extension MainQueueDispatchDecorator: FeedImageDataLoader where T == FeedImageDataLoader {
+  func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+    decoratee.loadImageData(from: url) { [weak self] result in
       self?.dispatch {
         completion(result)
       }
--- SuperFeed/SuperFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
+++ SuperFeed/SuperFeediOSTests/FeedUI/FeedUIIntegrationTests.swift
@@ -267,6 +267,22 @@ final class FeedUIIntegrationTests: XCTestCase {
     wait(for: [exp], timeout: 1.0)
   }
   
+  func test_loadImageDataCompletion_dispatchesFromBackgroundToMainThread() {
+    let (sut, loader) = makeSUT()
+    sut.loadViewIfNeeded()
+    
+    loader.completeFeedLoading(with: [makeImage()])
+    _ = sut.simulateFeedImageViewVisible(at: 0)
+    
+    let exp = expectation(description: "Wait for background queue")
+    DispatchQueue.global().async {
+      loader.completeImageLoading(with: self.anyImageData(), at: 0)
+      exp.fulfill()
+    }
+    
+    wait(for: [exp], timeout: 1.0)
+  }
+  
   // MARK: - Helpers
   
   private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedViewController, loader: LoaderSpy) {
@@ -302,6 +318,10 @@ final class FeedUIIntegrationTests: XCTestCase {
   
   private func makeImage(description: String? = nil, location: String? = nil, url: URL = URL(string: "http://any-url.com")!) -> FeedImage {
     FeedImage(id: UUID(), description: description, location: location, url: url)
+  }
+  
+  private func anyImageData() -> Data {
+    UIImage.make(withColor: .red).pngData()!
   }
   
   class LoaderSpy: FeedLoader, FeedImageDataLoader {
