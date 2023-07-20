bc1c1bcda6c8488c476a24067173edae985333dd
Yossa Bourne <cybercanonx@gmail.com>
2023-07-21 00:10:05 +0700

    add test for title

---
+++ SuperFeed/SuperFeed/FeedPresentation/FeedPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  FeedPresenter.swift
+//  SuperFeed
+//
+//  Created by yossa on 21/7/2566 BE.
+//
+
+import Foundation
--- SuperFeed/SuperFeedTests/FeedPresentation/FeedPresenterTests.swift
+++ SuperFeed/SuperFeedTests/FeedPresentation/FeedPresenterTests.swift
@@ -42,6 +42,14 @@ protocol FeedErrorView {
 
 final class FeedPresenter {
   
+  static var title: String {
+    NSLocalizedString(
+      "FEED_VIEW_TITLE",
+      tableName: "Feed",
+      bundle: Bundle(for: FeedPresenter.self),
+      comment: "Title for the feed view")
+  }
+  
   private let feedView: FeedView
   private let loadingView: FeedLoadingView
   private let errorView: FeedErrorView
@@ -77,6 +85,10 @@ final class FeedPresenter {
 }
 
 final class FeedPresenterTests: XCTestCase {
+  
+  func test_title_isLocalized() {
+    XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+  }
   
   func test_init_doesNotSendMessagesToView() {
     let (_, view) = makeSUT()
