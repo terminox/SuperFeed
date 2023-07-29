acc54a0a61abada8cf2f195e3d1ab64d0c68eeea
Yossa Bourne <cybercanonx@gmail.com>
2023-07-29 23:38:40 +0700

    update `CoreDataFeedStore+FeedImageDataLoader`

--- SuperFeed/SuperFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ SuperFeed/SuperFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -11,12 +11,18 @@ extension CoreDataFeedStore: FeedImageDataStore {
   public func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
     perform { context in
       completion(Result {
-//        try ManagedFeedImage.firs
+        try ManagedFeedImage.first(with: url, in: context)
+          .map { $0.data = data }
+          .map(context.save)
       })
     }
   }
   
   public func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {
-    <#code#>
+    perform { context in
+      completion(Result {
+        try ManagedFeedImage.first(with: url, in: context)?.data
+      })
+    }
   }
 }
