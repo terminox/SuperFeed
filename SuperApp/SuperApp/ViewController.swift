d6a533e6edb671118ee4d57e304be36691f177a7
Yossa Bourne <cybercanonx@gmail.com>
2023-07-30 02:30:25 +0700

    create an empty SuperApp project

---
+++ SuperApp/SuperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperApp
+//
+//  Created by yossa on 30/7/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
