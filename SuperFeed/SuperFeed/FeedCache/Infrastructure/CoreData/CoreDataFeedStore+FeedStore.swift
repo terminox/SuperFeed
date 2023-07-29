47e3e99c9c0374f433788d622f416dc374dde039
Yossa Bourne <cybercanonx@gmail.com>
2023-07-29 23:50:29 +0700

    update `CoreDataFeedStore+FeedStore` implementation

--- SuperFeed/SuperFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
+++ SuperFeed/SuperFeed/FeedCache/Infrastructure/CoreData/CoreDataFeedStore+FeedStore.swift
@@ -13,17 +13,30 @@ extension CoreDataFeedStore: FeedStore {
       completion(Result {
         try ManagedCache.find(in: context)
           .map {
-//            CachedFeed
+            CachedFeed(feed: $0.localFeed, timestamp: $0.timestamp)
           }
       })
     }
   }
   
-  public func insert(_ items: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-    <#code#>
+  public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+    perform { context in
+      completion(Result {
+        let managedCache = try ManagedCache.newUniqueInstance(in: context)
+        managedCache.timestamp = timestamp
+        managedCache.feed = ManagedFeedImage.images(from: feed, in: context)
+        try context.save()
+      })
+    }
   }
   
   public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-    <#code#>
+    perform { context in
+      completion(Result {
+        try ManagedCache.find(in: context)
+          .map(context.delete)
+          .map(context.save)
+      })
+    }
   }
 }
