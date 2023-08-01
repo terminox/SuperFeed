5a28aebd73093c079b8745ea68715324678c69af
Yossa Bourne <cybercanonx@gmail.com>
2023-08-01 15:04:39 +0700

    `FeedLoaderWithFallbackComposite` delivers error on both loaders failure

--- SuperApp/SuperAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ SuperApp/SuperAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -58,6 +58,12 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
 
     expect(sut, toCompleteWith: .success(fallbackFeed))
   }
+  
+  func test_load_deliversErrorOnBothPrimaryAndFallbackFailure() {
+    let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .failure(anyNSError()))
+
+    expect(sut, toCompleteWith: .failure(anyNSError()))
+  }
 
   // MARK: Private
 
